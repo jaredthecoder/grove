c7e7b9f2e5804abf79690926259674160a1a49ee
jaredmichaelsmith <jared@jaredsmith.io>
2016-04-27 22:53:29 -0400

    Update settings

--- api/settings.py
+++ api/settings.py
@@ -11,18 +11,13 @@ class Config(object):
     TESTING = False
     APP_DIR = os.path.abspath(os.path.dirname(__file__))
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
-    CACHE_TYPE = 'simple'
-    BCRYPT_LOG_ROUNDS = 13
-    CSRF_ENABLED = True
-    SECURITY_PASSWORD_HASH = 'bcrypt'
-    SECURITY_TRACKABLE = True
-    SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authorization'
 
-    MONGODB_DB = os.environ.get('WHERENO_DB', 'whereno')
+    # Local Settings
+    """MONGODB_DB = os.environ.get('WHERENO_DB', 'whereno')
     MONGODB_HOST = os.environ.get('WHERENO_HOST', 'localhost')
     MONGODB_PORT = os.environ.get('WHERENO_PORT', 27017)
     MONGODB_USERNAME = os.environ.get('WHERENO_USERNAME', 'whereno')
-    MONGODB_PASSWORD = os.environ.get('WHERENO_PASSWORD', 'whereno')
+    MONGODB_PASSWORD = os.environ.get('WHERENO_PASSWORD', 'whereno')"""
 
 
 class ProdConfig(Config):
