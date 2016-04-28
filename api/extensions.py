e93d4b2c32466f1447c0872624eb52a19dd7cbd6
jaredmichaelsmith <jared@jaredsmith.io>
2016-04-27 23:02:12 -0400

    Add back extensions

--- api/extensions.py
+++ api/extensions.py
@@ -2,7 +2,11 @@
 """Extensions module. Each extension is initialized in the app factory located
 in app.py."""
 
+from flask.ext.bcrypt import Bcrypt
+from flask.ext.cache import Cache
 from flask.ext.mongoengine import MongoEngine
 
+bcrypt = Bcrypt()
+cache = Cache()
 db = MongoEngine()
 
